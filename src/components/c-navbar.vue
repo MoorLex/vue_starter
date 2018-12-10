89269922dc6b83f092c1485412576cc56b80b080
moorlex <moorlex@mail.com>
2018-12-10 14:06:47 +0300

    Start

---
+++ src/components/c-navbar.vue
@@ -0,0 +1,26 @@
+<template>
+	<nav class="topbar py-3 position-absolute">
+		<div class="container">
+
+			<a class="topbar-brand" href="#">Navbar w/ text</a>
+
+			<ul class="nav topbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Features</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Pricing</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</template>
+
+<script>
+    export default {
+        name: 'ComponentNavbar'
+    }
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-page">
+    <c-navbar/>
+    <s-header/>
+  </div>
+</template>
+
+<script>
+  import CNavbar from '../components/c-navbar'
+  import SHeader from '../sections/s-header'
+
+  export default {
+    name: 'HomePage',
+    components: {
+        CNavbar,
+        SHeader
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped></style>
